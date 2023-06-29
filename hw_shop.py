6a38eed814356e2e1188606b7744b9e056cf2878
waterzaddy <kelli.garnett@pm.me>
2023-06-30 00:07:02 +0100

    Added purchasing choice

--- hw_shop.py
+++ hw_shop.py
@@ -19,6 +19,18 @@ def welcome_message():
     for item in prices:
         print(f"{item} - £{prices.get(item)}")
 
+    choice = input("\nWould you like to buy anything?\nEnter 'exit' to exit the shop\n")
+
+    if choice == "exit":
+        exit(0)
+    elif choice in prices.keys():
+        if purchase(choice, customer_balance):
+            goodbye_greeting(choice)
+        else:
+            retry_purchase(choice, customer_balance)
+    else:
+        raise ValueError("Not a valid answer, please try again")
+
 
 def goodbye_greeting(item):
     print(f"Here is your {item}!\n See you again soon!")
